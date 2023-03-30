ebd56f3ecd123a25723a571603af5e3da3e37cd0
Vlad8284 <Vlad555333@maul.ru>
2023-03-30 13:28:07 +0300

    Changed the folder Homework

--- Homework/scripts.js
+++ Homework/scripts.js
@@ -13,9 +13,34 @@ alert(javaScript);
 let x = 10;
 let y = 2;
 
-alert(x+y);
-alert(x-y);
-alert(x*y);
-alert(x/y);
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
 
-alert(y**5);
+alert(y ** 5);
+
+a = a - 11;
+let b = 2;
+
+alert(a / b);
+
+let i = 1;
+i = i + 5;
+i = i - 3;
+i = i * 7;
+i = i / 3;
+i = i + 1;
+i = i - 1;
+alert(i);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+const user = {
+    name: 'Миша',
+    age: '35',
+    isAdmin: 'Yes or no ?'
+
+}
+
