3bc2fe45af5bf10e05325dd1a231bd90392c4922
Vlad8284 <Vlad555333@maul.ru>
2023-04-12 12:03:26 +0300

    Changed the file JS

--- Homework/scripts.js
+++ Homework/scripts.js
@@ -101,4 +101,52 @@ let e = 50;
 
 (d > 100 || e > 100) ? console.log('Верно') : console.log('Не верно');
 
- 
+let a = '2';
+let b = '3';
+alert(+a + +b);
+
+
+
+
+
+let monthNumber = String(prompt('Введите номер месяца'));
+
+
+
+switch (monthNumber) {
+
+    case '1':
+    case '2':
+    case '12':
+    
+        console.log('Зима');
+
+        break;
+
+    case '3':
+    case '4': 
+    case '5':
+
+         console.log('Весна');
+
+         break;
+       
+    case '6':
+    case '7': 
+    case '8':
+
+         console.log('Лето');
+
+         break; 
+         
+    case '9':
+    case '10': 
+    case '11':
+
+         console.log('Осень');
+
+         break;
+        default:
+    console.log('номер месяца больше 13');
+        break;
+}
