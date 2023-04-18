05e9ca832b3acde40d19b9912a85bfac5bb0a22c
Vlad8284 <Vlad555333@maul.ru>
2023-04-18 14:16:47 +0300

    Changed the file JS

--- Homework/scripts/scripts.js
+++ Homework/scripts/scripts.js
@@ -32,39 +32,39 @@ alert(+a + +b);
 
 
 
-let monthNumber = String(prompt('Введите номер месяца'));
+let monthNumber = Number(prompt('Введите номер месяца'));
 
 
 
 switch (monthNumber) {
 
-    case '1':
-    case '2':
-    case '12':
+    case 1:
+    case 2:
+    case 12:
 
         console.log('Зима');
 
         break;
 
-    case '3':
-    case '4':
-    case '5':
+    case 3:
+    case 4:
+    case 5:
 
         console.log('Весна');
 
         break;
 
-    case '6':
-    case '7':
-    case '8':
+    case 6:
+    case 7:
+    case 8:
 
         console.log('Лето');
 
         break;
 
-    case '9':
-    case '10':
-    case '11':
+    case 9:
+    case 10:
+    case 11:
 
         console.log('Осень');
 
