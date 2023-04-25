e4ee39282367a42287de02fadb244759e449125b
Vlad8284 <Vlad555333@maul.ru>
2023-04-25 21:04:00 +0300

    Changed the file JS

--- Homework/scripts/scripts.js
+++ Homework/scripts/scripts.js
@@ -1,79 +1,3 @@
-
-
-let password = 'Hello Friend';
-let enterThePassword = String(prompt('Введите пароль'));
-
-if (password === enterThePassword) {
-    alert('Пароль введен верно');
-} else {
-    alert('Пароль введен неправильно');
-}
-
-let c = 20;
-
-if (c > 0 && c < 10) {
-    console.log('Верно');
-} else {
-    console.log('Не верно');
-}
-
-(c > 0 && c < 10) ? console.log('Верно') : console.log('Не верно');
-
-let d = 250;
-let e = 50;
-
-(d > 100 || e > 100) ? console.log('Верно') : console.log('Не верно');
-
-let a = '2';
-let b = '3';
-alert(+a + +b);
-
-
-
-
-
-let monthNumber = Number(prompt('Введите номер месяца'));
-
-
-
-switch (monthNumber) {
-
-    case 1:
-    case 2:
-    case 12:
-
-        console.log('Зима');
-
-        break;
-
-    case 3:
-    case 4:
-    case 5:
-
-        console.log('Весна');
-
-        break;
-
-    case 6:
-    case 7:
-    case 8:
-
-        console.log('Лето');
-
-        break;
-
-    case 9:
-    case 10:
-    case 11:
-
-        console.log('Осень');
-
-        break;
-    default:
-        console.log('номер месяца больше 13');
-        break;
-}
-
 for (let i = 0; i <= 2; i++) { console.log("Привет!"); }
 
 let i = 1;
@@ -122,7 +46,7 @@ const obj = {
 
 for (const key in obj) {
 
-    console.log (`${key} - зарплата ${obj[key]} долларов`);
+    console.log(`${key} - зарплата ${obj[key]} долларов`);
 
 }
 
