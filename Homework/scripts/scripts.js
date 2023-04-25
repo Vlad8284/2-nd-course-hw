39eae797f72f5b4ca165cf67cde0f6b801ceabf9
Vlad8284 <Vlad555333@maul.ru>
2023-04-25 20:53:12 +0300

    Changed the file JS

--- Homework/scripts/scripts.js
+++ Homework/scripts/scripts.js
@@ -108,6 +108,22 @@ for (k = 7; k <= 22; k++) {
     console.log(k);
 }
 
+const obj = {
 
+    Коля: 200,
+
+    Вася: 300,
+
+    Петя: 400,
+
+}
+
+
+
+for (const key in obj) {
+
+    console.log (`${key} - зарплата ${obj[key]} долларов`);
+
+}
 
 
