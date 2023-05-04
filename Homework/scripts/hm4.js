79b6a2065f5f0e2f63a609a1b6a2f9d65b3d3383
Vlad8284 <Vlad555333@maul.ru>
2023-05-04 14:55:17 +0300

    Changed the file hm4.js

---
+++ Homework/scripts/hm4.js
@@ -0,0 +1,10 @@
+// Задание 1
+
+let a = prompt("Введите первое число");
+let b = prompt("Введите второе число");
+
+function comparison(a, b) {
+    return a < b ? a : b;
+}
+
+console.log(comparison(a, b));
