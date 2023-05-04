2571e2dbc80bedded6388ef2fe94f5e2097aab3f
Vlad8284 <Vlad555333@maul.ru>
2023-05-04 11:55:36 +0300

    Changed the file JS

---
+++ Homework/scripts/hm1.js
@@ -0,0 +1,106 @@
+// Задание 1
+
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+// Задание 2
+
+const iphone = 2007;
+alert(iphone);
+
+// Задание 3
+
+const javaScript = "Brendan Eich";
+alert(javaScript);
+
+// Задание 4
+
+let x = 10;
+let y = 2;
+
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
+
+// Задание 5
+
+alert(y ** 5);
+ 
+// Задание 6
+
+a = a - 11;
+let b = 2;
+
+alert(a % b);
+
+// Задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let i = 1;
+i += 5;
+i -= 3;
+i *= 7;
+i /= 3;
+++i;
+--i;
+alert(i);
+
+// Задание 8
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+// Задание 9.0
+
+const user =
+{
+    name: 'Миша',
+    age: age,
+    isAdmin: false
+}
+
+// Задание 9.1
+
+user ['city of residence'] = 'Владимир';
+
+// Задание 9.2
+
+user.age ['(age = String)'] = true; 
+console.log(user.age);
+console.log(user['age']);
+
+// Задание 9.3
+
+delete user ['city of residence'];
+
+// Задание 9.4
+
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[info]);
+
+// Задание 10
+
+const name = prompt('Как, Ваше ИМЯ?');
+
+alert(`Привет, ${name} !`);
+
+
+    
+    
+  
+
+
+
+ 
